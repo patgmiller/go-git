d39fecc8ae964f32c5316667acec6b0a041724db
Patrick Miller <patrick.miller@tcn.com>
2023-11-04 13:25:49 -0600

    merge, fast-forward: add stub merge_test.go go-git/go-git#905

---
+++ merge_test.go
@@ -0,0 +1,17 @@
+package git
+
+import . "gopkg.in/check.v1"
+
+type MergeTestSuite struct {
+	BaseSuite
+}
+
+var _ = Suite(&MergeTestSuite{})
+
+// func (s *MergeTestSuite) SetUpTest(c *C) {
+
+// }
+
+func (s *MergeTestSuite) TestMergeFastForward(c *C) {
+
+}
